87a180eb9aa5e934065835f48cf89f70c912c9ff
Isaac Rowntree <isaac.rowntree@redbubble.com>
2017-05-12 13:51:44 +1000

    Step 3: Fire an action from the store and catch it in the reducer
     - Build a simple counter which fires once on page load
     - Add a subscriber which is essentially the same as the devtools
       extension but allows you to poll for changes in an alternate way

--- src/index.js
+++ src/index.js
@@ -93,9 +93,20 @@ class Game extends React.Component {
   }
 }
 
-const reducer = (state = {}, action) => state;
+const reducer = (state = {count: 0}, action) => {
+  if (action.type === 'INCREMENT') {
+    return Object.assign({}, state, {count: state.count + 1});
+  }
+  return state;
+};
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const action = {type: 'INCREMENT'};
+
+store.subscribe(() => {console.log(store.getState())});
+
+store.dispatch(action);
 
 // ========================================
 
